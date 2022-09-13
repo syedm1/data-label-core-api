101eaa44d48beab9746f21ca5f04a90ac14af8c4
syedm1 <31075630+syedm1@users.noreply.github.com>
2022-09-13 20:36:37 +1000

    Add flask dummy API

---
+++ colors.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify
+from flasgger import Swagger
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+# goto http://localhost:5000/apidocs/#/default/get_colors__palette__
+
+@app.route('/colors/<palette>/')
+def colors(palette):
+    # API SPEC DOCUMENTATION
+    """Example endpoint returning a list of colors by palette
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: A list of colors (may be filtered by palette)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+
+    all_colors = {
+        'cmyk': ['cyan', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
+
+    return jsonify(result)
+
+app.run(debug=True)
